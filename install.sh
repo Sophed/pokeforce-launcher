be2fbfa6d74b8a16a6885861b6ea014ddaf3cff4
Sophia <65602967+Sophed@users.noreply.github.com>
2024-08-27 23:02:40 +0100

    please work oh my god

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-curl -o pokeforce https://github.com/Sophed/pokeforce-launcher/releases/download/v0.1/launcher-x86
-chmod +x pokeforce
-sudo mv pokeforce /usr/bin/
+wget https://github.com/Sophed/pokeforce-launcher/releases/download/v0.1/launcher-x86
+chmod +x launcher-x86
+sudo mv launcher-x86 /usr/bin/pokeforce
 curl -o pokeforce.desktop https://raw.githubusercontent.com/Sophed/pokeforce-launcher/main/pokeforce.desktop
 sudo mv pokeforce.desktop /usr/share/applications/
 echo "successfully installed! run 'pokeforce' to start."
