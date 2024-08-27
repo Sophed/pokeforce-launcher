1cdd1e238cbfcd3f11c6984667cd9819daf3ff01
Sophia <65602967+Sophed@users.noreply.github.com>
2024-08-27 22:55:36 +0100

    update install script

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ fi
 curl -o pokeforce https://github.com/Sophed/pokeforce-launcher/releases/download/v0.1/launcher-x86
 chmod +x pokeforce
 sudo mv pokeforce /usr/bin/
+curl -o pokeforce.desktop https://raw.githubusercontent.com/Sophed/pokeforce-launcher/main/pokeforce.desktop
+sudo mv pokeforce.desktop /usr/share/applications/
 echo "successfully installed! run 'pokeforce' to start."
